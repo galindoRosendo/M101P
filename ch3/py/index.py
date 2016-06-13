f745349d21672d6dd6945ded117eb418903ab216
Rosendo Galindo <rosendo_galindo@hotmail.com>
2016-06-13 02:38:00 -0500

    chapter 3 homework done

--- ch3/py/index.py
+++ ch3/py/index.py
@@ -1,27 +1,36 @@
 import pymongo
-import datetime
 import sys
 
-# establish a connection to the database
 connection = pymongo.MongoClient("mongodb://localhost")
+db = connection.school
+students = db.students
 
-# add a review date to a single record using update_one
-def viewOneStudentHW(student_id):
+def remove_lowest_homework_score( scores):
+	homework_scores = [score for score in scores if score[u"type"] == u"homework"]
+ 	lowest = min( [score[u"score"] for score in homework_scores])
+ 	return [score for score in scores \
+ 		if ( score[u"type"] != u"homework") \
+ 		   or( score[u"type"] == u"homework" and score[u"score"] != lowest)]
 
-    print "Show one student"
-    # get a handle to the school database
-    db=connection.school
-    students = db.students
+def update_scores( coll, doc_id, new_scores):
+	try:
+		coll.update( { "_id": doc_id}, { "$set": { "scores": new_scores }})
+	except:
+		print "Unexpected error while updating:", sys.exc_info()[ 0]
 
-    try:
-        # get the doc
-        score = students.find_one({'_id':student_id})
-        print "Student: ", score
+def main( argv):
+	try:
+		# All students having a score of type "homework" in their scores array
+		cursor = students.find( { 'scores.type': 'homework' })
+	except:
+		print "Unexpected error while finding:", sys.exc_info()[ 0]
 
-    except Exception as e:
-        raise
+	for doc in cursor:
+	 	id = doc[ "_id"]
+	 	scores = doc[ "scores"]
+	 	updated_scores = remove_lowest_homework_score( scores)
+	 	update_scores( students, id, updated_scores)
+	 	print "updated doc %s:\n    scores: %s\nnew scores: %s" % ( id, scores, updated_scores)
 
-viewOneStudentHW(137)
-viewOneStudentHW(138)
-viewOneStudentHW(139)
-viewOneStudentHW(140)
+if __name__ == "__main__":
+	main(sys.argv[1:])
